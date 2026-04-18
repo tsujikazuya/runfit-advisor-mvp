d9f7d33da830e4704e1b8b46764a016c6b0c25e2
tsujikazuya <tsuji.kazuya@gmail.com>
2026-04-18 11:40:42 +0900

    Fix FormEvent import and build for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useMemo, useState } from 'react'
+import { useMemo, useState } from 'react'
+import type { FormEvent } from 'react'
 import { Link, Navigate, Route, Routes, useNavigate, useParams } from 'react-router-dom'
 
 type Customer = {
